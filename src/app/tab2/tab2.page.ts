e9c2827cf0bb45ff10d57efd8988411586272d09
Zewei Xia <xiazeweibobby@gmail.com>
2025-04-12 17:14:03 +0800

    Fix the routing problems with Tab2
    Fix some routing issues and finally run Tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { DataService } from './data.service';
-import { ToastController } from '@ionic/angular';
+import {IonicModule, ToastController} from '@ionic/angular';
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
+  imports: [CommonModule, FormsModule, IonicModule]
 })
 export class Tab2Page {
   newItem: any = {
