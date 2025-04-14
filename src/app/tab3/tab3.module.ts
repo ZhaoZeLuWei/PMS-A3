8d417d36485a2b7792a33d8b8c6f76d674a35463
Zewei Xia <xiazeweibobby@gmail.com>
2025-04-14 14:11:08 +0800

    Service & search
    Fetch all items in URL
    Search for items

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -2,7 +2,7 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { Tab3Page } from './tab3.page';
+
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
@@ -14,8 +14,7 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
         FormsModule,
         ExploreContainerComponentModule,
         Tab3PageRoutingModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
     ],
-  declarations: [Tab3Page]
 })
 export class Tab3PageModule {}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {IonicModule} from "@ionic/angular";
+import {Router, RouterModule} from "@angular/router";
 import {ActionSheetController} from "@ionic/angular";
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AlertController} from "@ionic/angular";
-import {Observable} from "rxjs";
+import {Tab3Service} from "./tab3.service";
+import {Item} from "./tab3Item.model";
+import {CommonModule} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
 
 interface Params {
   id: number;
@@ -12,18 +17,23 @@ interface Params {
 
 @Component({
   selector: 'app-tab3',
+  standalone: true,
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  standalone: false,
+  imports: [FormsModule, IonicModule, ReactiveFormsModule, RouterModule, CommonModule],
 })
-export class Tab3Page {
+
+export class Tab3Page implements OnInit {
+  items: FormGroup[] = [];
+  resultItems: FormGroup[] = [];
   inputform: FormGroup;
   params: Params;
   searchSuccess: boolean = false;
   deleteSuccess: boolean = true;
 
   constructor(private router:Router, private actionSheetCtrl: ActionSheetController,
-              private fb: FormBuilder, private ac:AlertController) {
+              private fb: FormBuilder, private ac:AlertController, private http:HttpClient,
+              private t3Service: Tab3Service) {
     this.params = {id:1,name:"Bob"};
     this.inputform = fb.group({
       itemName: ['', Validators.required],
@@ -70,5 +80,45 @@ export class Tab3Page {
     await actionSheet.present();
   }
 
+  ngOnInit() {
+    this.t3Service.getItems().subscribe({
+      next: (scuData: Item[]) => {
+        this.items = scuData.map(
+          item => this.fb.group({
+            id: [item.item_id],
+            name: [item.item_name],
+            category: [item.category],
+            quantity: [item.quantity],
+            price: [item.price],
+            supplierName: [item.supplier_name],
+            stockStatus: [item.stock_status],
+            featuredItem: [item.featured_item],
+            specialNote : [item.special_note || ""],
+          })
+        );
+        console.log(this.items);
+        this.resultItems = this.items;
+      },
+      error: (err: any) => {
+        console.error("Error to init the items form group.", err);
+      }
+    })
+  }
+
+  onSearch() {
+      let searchItemName = this.inputform.get('itemName')?.value.toLowerCase();
+      if (searchItemName) {
+        this.resultItems = this.items.filter(item => {
+          return item.get('name')?.value.toLowerCase().includes(searchItemName);
+        });
+        if(this.resultItems.length > 0)  {
+          this.searchSuccess = true;
+          console.log(this.resultItems);
+        }
+      }else {
+        this.resultItems = this.items;
+        this.searchSuccess = false;
+      }
+  }
 }
 
