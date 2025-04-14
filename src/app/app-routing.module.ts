b80a199ee4d66edb1d266d1d53c9d89e78f5ce56
Zewei Xia <xiazeweibobby@gmail.com>
2025-04-15 00:05:45 +0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {
     path: '',
@@ -12,6 +13,10 @@ const routes: Routes = [
   },{
     path:'tab3',
     loadChildren: () => import('./tab3/tab3.module').then(m => m.Tab3PageModule)
+  },
+  {
+    path: 'tab3/:id',
+    loadChildren: () => import('./name/name.module').then(m => m.NamePageModule)
   }
 ];
 @NgModule({
